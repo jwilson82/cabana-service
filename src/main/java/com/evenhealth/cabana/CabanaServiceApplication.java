79066c3a3323a3b228348d7b477ab4542f452887
Jeff Wilson <jeff.r.wilson@gmail.com>
2022-04-29 19:25:16 -0400

    start spring io

---
+++ src/main/java/com/evenhealth/cabana/CabanaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evenhealth.cabana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabanaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabanaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evenhealth/cabana/CabanaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenhealth.cabana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabanaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
